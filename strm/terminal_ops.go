f57ef211d20ba02fdc384cfbbeb29493dfcd104a
Pedro Costa <pedro.costa2@sky.uk>
2022-03-23 14:38:14 +0000

    updates documentation; adds SumBy

--- strm/terminal_ops.go
+++ strm/terminal_ops.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 )
 
+// ToSlice returns a Slice containing all the elements of this Stream
 func (s *Stream[T]) ToSlice() []T {
 	return s.filteredSlice()
 }
 
-func (s *Stream[T]) ForEach(f func(T)) {
+// ForEach Performs the given action on each element of the Stream
+func (s *Stream[T]) ForEach(action func(T)) {
 	for _, elem := range s.filteredSlice() {
-		f(elem)
+		action(elem)
 	}
 }
 
+// Any Returns true if at least one element matches the given predicate.
 func (s *Stream[T]) Any(p predicate[T]) bool {
 	for _, elem := range s.filteredSlice() {
 		if p(elem) {
@@ -23,6 +26,7 @@ func (s *Stream[T]) Any(p predicate[T]) bool {
 	return false
 }
 
+// All Returns `true` if all elements match the given predicate.
 func (s *Stream[T]) All(p predicate[T]) bool {
 	for _, elem := range s.filteredSlice() {
 		if !p(elem) {
@@ -32,14 +36,17 @@ func (s *Stream[T]) All(p predicate[T]) bool {
 	return true
 }
 
+// None Returns true if no elements match the given predicate.
 func (s *Stream[T]) None(p predicate[T]) bool {
 	return !s.Any(p)
 }
 
+// Count Returns the number of elements in this Stream
 func (s *Stream[T]) Count() int {
 	return len(s.filteredSlice())
 }
 
+// CountBy Returns the number of elements matching the given predicate [p].
 func (s *Stream[T]) CountBy(p predicate[T]) (count int) {
 	for _, elem := range s.filteredSlice() {
 		if p(elem) {
@@ -49,6 +56,15 @@ func (s *Stream[T]) CountBy(p predicate[T]) (count int) {
 	return
 }
 
+// SumBy Returns the sum of all values produced by [selector] function applied to each element in the Stream.
+func (s *Stream[T]) SumBy(selector func(t T) int) (sum int) {
+	for _, elem := range s.filteredSlice() {
+		sum += selector(elem)
+	}
+	return
+}
+
+// FirstBy Returns the first element of this Stream matching the given predicate [p].
 func (s *Stream[T]) FirstBy(p predicate[T]) (t T) {
 	for _, elem := range s.filteredSlice() {
 		if p(elem) {
@@ -58,6 +74,7 @@ func (s *Stream[T]) FirstBy(p predicate[T]) (t T) {
 	return
 }
 
+// First Returns the first element of this Stream
 func (s *Stream[T]) First() (t T) {
 	if len(s.filteredSlice()) == 0 {
 		return
@@ -65,6 +82,7 @@ func (s *Stream[T]) First() (t T) {
 	return s.slice[0]
 }
 
+// Last Returns the last element of this Stream
 func (s *Stream[T]) Last() (t T) {
 	if len(s.filteredSlice()) == 0 {
 		return
@@ -72,6 +90,7 @@ func (s *Stream[T]) Last() (t T) {
 	return s.slice[len(s.slice)-1]
 }
 
+// JoinToString Creates a string from all the elements separated using [separator]
 func (s *Stream[T]) JoinToString(delimiter string) (joined string) {
 	for i := 0; i < len(s.filteredSlice()); i++ {
 		joined += fmt.Sprint(s.slice[i])
